5e3b133cfe3a77cde9bcd24652b146cb0c94dbb9
Hemanth0411 <hemanthreddyannem@gmail.com>
2023-11-25 09:23:53 +0530

    Made it a bit easier.

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,5 +1,10 @@
 import random
 
+print("Welcome to Number Guessing Game!")
+print()
+name=input("Enter your name:")
+print("Hello, ",name)
+print("Let the game Begin:")
 true_number=random.randint(1,100)
 guess_number=int(input("Enter your guess between 1 and 100: "))
 
@@ -8,10 +13,18 @@ while True:
         print("You are right! Good Job.")
         break
     elif guess_number>true_number:
-        print("Your guess is too high, Please try again.")
+        x=abs(guess_number-true_number)
+        if x>10:
+            print("Your guess is too high, Please try again.")
+        else:
+            print("Your guess is just a bit higher, try again.")
         guess_number=int(input("Enter your guess between 1 and 100: "))
 
     elif guess_number<true_number:
-        print("Your guess is too low, Please try again.")
+        x=abs(guess_number-true_number)
+        if x>10:
+            print("Your guess is too low, Please try again.")
+        else:
+            print("Your guess is just a little low, try again.")
         guess_number=int(input("Enter your guess between 1 and 100: "))
         
