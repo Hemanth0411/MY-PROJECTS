64bedeb1503cf11a4fca0b3fc360253c270fcc5c
Hemanth0411 <hemanthreddyannem@gmail.com>
2023-11-27 15:01:30 +0530

    Added a few spaces and newlines along with another while loop so that it can work continuosly and a break statement to exit the loop if the user decides not to exit the program anymore.

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -7,21 +7,28 @@ def mult(x, y):
 def div(x, y):
   return x/y
 
-print(f"1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit")
-choice=input("Enter your choice: ")
 while True:
-  if choice in ('1','2','3','4'):
-    num1=float(input("Enter the first number: "))
-    num2=float(input("Enter the second number: "))
-
-    if choice == '1':
-      print(num1, "+", num2, "=", add(num1, num2))
-    elif choice =='2':
-      print(num1, "-", num2, "=", sub(num1, num2))
-    elif choice=='3':
-      print(num1, "*", num2, "=", mult(num1, num2))
-    elif choice =='4':
-        print(num1, "/", num2, "=", div(num1, num2))
+  print(f"1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exit")
+  choice=input("Enter your choice: ")
+  if choice=='5':
     break
-  else:
-    print("Invalid Input!")
+  while True:
+    if choice in ('1','2','3','4'):
+      num1=float(input("Enter the first number: "))
+      num2=float(input("Enter the second number: "))
+
+      if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+      elif choice =='2':
+        print(num1, "-", num2, "=", sub(num1, num2))
+      elif choice=='3':
+        print(num1, "*", num2, "=", mult(num1, num2))
+      elif choice =='4':
+        print(num1, "/", num2, "=", div(num1, num2))
+      break
+    else:
+      print(f"Invalid Input!\n")
+      break
+
+
+
